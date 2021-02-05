8ef3bf18bd44c100b759d8b93c7b5a5abd51b3a6
Yuri Gruschinsky <gruschinsky7@gmail.com>
2021-02-05 23:16:39 +0200

    change component App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from '../../img/logo.svg';
-import './App.scss';
+import style from './App.module.scss';
+
+import { Switch, Route } from 'react-router-dom';
+import { SearchScreen } from '../../screen/SearchScreen';
+import { ProfileScreen } from '../../screen/ProfileScreen';
+import { ROUTE } from '../../screen/config';
+import { AuthScreen } from '../../screen/AuthScreen';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.App}>
+      <Switch>
+        <Route exact path={ROUTE.home}>
+          <SearchScreen />
+        </Route>
+        <Route path={ROUTE.profile}>
+          <ProfileScreen />
+        </Route>
+        <Route path={ROUTE.auth}>
+          <AuthScreen />
+        </Route>
+      </Switch>
     </div>
   );
 };
