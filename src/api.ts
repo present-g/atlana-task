541d0dfbe5a2451e26b0129616784f8abadf8062
Yuri Gruschinsky <gruschinsky7@gmail.com>
2021-02-05 23:20:36 +0200

    config api

---
+++ src/api.ts
@@ -0,0 +1,9 @@
+// https://api.github.com/search/users?q=
+
+const GITHUB_API_TOKEN = 'GITHUB_API_TOKEN';
+
+const BASE_URL = 'https://api.github.com';
+const SEARCH_URL = '/search';
+const QUERY_URL = '?q=';
+
+export { BASE_URL, SEARCH_URL, QUERY_URL, GITHUB_API_TOKEN };
